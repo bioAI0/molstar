ca72f31f1e17007cef706bc006cd77495e255f86
sness <snessnet@gmail.com>
2024-12-21 21:17:15 -0800

    More

--- src/apps/docking-viewer/viewport.tsx
+++ src/apps/docking-viewer/viewport.tsx
@@ -4,10 +4,7 @@
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
 
-import { InteractionsRepresentationProvider } from '../../mol-model-props/computed/representations/interactions';
-import { InteractionTypeColorThemeProvider } from '../../mol-model-props/computed/themes/interaction-type';
 import { presetStaticComponent, StructureRepresentationPresetProvider } from '../../mol-plugin-state/builder/structure/representation-preset';
-import { StructureSelectionQueries, StructureSelectionQuery } from '../../mol-plugin-state/helpers/structure-selection-query';
 import { StructureRef } from '../../mol-plugin-state/manager/structure/hierarchy-state';
 import { PluginUIComponent } from '../../mol-plugin-ui/base';
 import { LociLabels } from '../../mol-plugin-ui/controls';
@@ -17,9 +14,7 @@ import { Viewport, ViewportControls } from '../../mol-plugin-ui/viewport';
 import { PluginCommands } from '../../mol-plugin/commands';
 import { PluginConfig } from '../../mol-plugin/config';
 import { PluginContext } from '../../mol-plugin/context';
-import { MolScriptBuilder as MS } from '../../mol-script/language/builder';
 import { StateObjectRef } from '../../mol-state';
-import { Color } from '../../mol-util/color';
 import { Material } from '../../mol-util/material';
 
 function shinyStyle(plugin: PluginContext) {
@@ -35,39 +30,6 @@ function shinyStyle(plugin: PluginContext) {
         }
     } });
 }
-
-function occlusionStyle(plugin: PluginContext) {
-    return PluginCommands.Canvas3D.SetSettings(plugin, { settings: {
-        renderer: {
-            ...plugin.canvas3d!.props.renderer,
-        },
-        postprocessing: {
-            ...plugin.canvas3d!.props.postprocessing,
-            outline: { name: 'on', params: {
-                scale: 1.0,
-                threshold: 0.33,
-                color: Color(0x0000),
-                includeTransparent: true,
-            } },
-            shadow: { name: 'off', params: {} },
-        }
-    } });
-}
-
-const ligandPlusSurroundings = StructureSelectionQuery('Surrounding Residues (5 \u212B) of Ligand plus Ligand itself', MS.struct.modifier.union([
-    MS.struct.modifier.includeSurroundings({
-        0: StructureSelectionQueries.ligand.expression,
-        radius: 5,
-        'as-whole-residues': true
-    })
-]));
-
-const ligandSurroundings = StructureSelectionQuery('Surrounding Residues (5 \u212B) of Ligand', MS.struct.modifier.union([
-    MS.struct.modifier.exceptBy({
-        0: ligandPlusSurroundings.expression,
-        by: StructureSelectionQueries.ligand.expression
-    })
-]));
 
 const PresetParams = {
     ...StructureRepresentationPresetProvider.CommonParams,
@@ -102,147 +64,21 @@ export const StructurePreset = StructureRepresentationPresetProvider({
     }
 });
 
-export const IllustrativePreset = StructureRepresentationPresetProvider({
-    id: 'preset-illustrative',
-    display: { name: 'Illustrative' },
-    params: () => PresetParams,
-    async apply(ref, params, plugin) {
-        const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
-        if (!structureCell) return {};
-
-        const components = {
-            ligand: await presetStaticComponent(plugin, structureCell, 'ligand'),
-            polymer: await presetStaticComponent(plugin, structureCell, 'polymer'),
-        };
-
-        const { update, builder, typeParams } = StructureRepresentationPresetProvider.reprBuilder(plugin, params);
-        const representations = {
-            ligand: builder.buildRepresentation(update, components.ligand, { type: 'spacefill', typeParams: { ...typeParams, ignoreLight: true }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ligand' }),
-            polymer: builder.buildRepresentation(update, components.polymer, { type: 'spacefill', typeParams: { ...typeParams, ignoreLight: true }, color: 'illustrative', colorParams: { palette: (plugin.customState as any).colorPalette } }, { tag: 'polymer' }),
-        };
-
-        await update.commit({ revertOnError: true });
-        await occlusionStyle(plugin);
-        plugin.managers.interactivity.setProps({ granularity: 'residue' });
-
-        return { components, representations };
-    }
-});
-
-const SurfacePreset = StructureRepresentationPresetProvider({
-    id: 'preset-surface',
-    display: { name: 'Surface' },
-    params: () => PresetParams,
-    async apply(ref, params, plugin) {
-        const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
-        const structure = structureCell?.obj?.data;
-        if (!structureCell || !structure) return {};
-
-        const components = {
-            ligand: await presetStaticComponent(plugin, structureCell, 'ligand'),
-            polymer: await presetStaticComponent(plugin, structureCell, 'polymer'),
-        };
-
-        const { update, builder, typeParams } = StructureRepresentationPresetProvider.reprBuilder(plugin, params);
-        const representations = {
-            ligand: builder.buildRepresentation(update, components.ligand, { type: 'ball-and-stick', typeParams: { ...typeParams, material: CustomMaterial, sizeFactor: 0.26 }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ligand' }),
-            polymer: builder.buildRepresentation(update, components.polymer, { type: 'molecular-surface', typeParams: { ...typeParams, material: CustomMaterial, quality: 'custom', resolution: 0.5, doubleSided: true }, color: 'partial-charge' }, { tag: 'polymer' }),
-        };
-
-        await update.commit({ revertOnError: true });
-        await shinyStyle(plugin);
-        plugin.managers.interactivity.setProps({ granularity: 'residue' });
-
-        return { components, representations };
-    }
-});
-
-const PocketPreset = StructureRepresentationPresetProvider({
-    id: 'preset-pocket',
-    display: { name: 'Pocket' },
-    params: () => PresetParams,
-    async apply(ref, params, plugin) {
-        const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
-        const structure = structureCell?.obj?.data;
-        if (!structureCell || !structure) return {};
-
-        const components = {
-            ligand: await presetStaticComponent(plugin, structureCell, 'ligand'),
-            surroundings: await plugin.builders.structure.tryCreateComponentFromSelection(structureCell, ligandSurroundings, `surroundings`),
-        };
-
-        const { update, builder, typeParams } = StructureRepresentationPresetProvider.reprBuilder(plugin, params);
-        const representations = {
-            ligand: builder.buildRepresentation(update, components.ligand, { type: 'ball-and-stick', typeParams: { ...typeParams, material: CustomMaterial, sizeFactor: 0.26 }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ligand' }),
-            surroundings: builder.buildRepresentation(update, components.surroundings, { type: 'molecular-surface', typeParams: { ...typeParams, material: CustomMaterial, includeParent: true, quality: 'custom', resolution: 0.2, doubleSided: true }, color: 'partial-charge' }, { tag: 'surroundings' }),
-        };
-
-        await update.commit({ revertOnError: true });
-        await shinyStyle(plugin);
-        plugin.managers.interactivity.setProps({ granularity: 'element' });
-
-        return { components, representations };
-    }
-});
-
-const InteractionsPreset = StructureRepresentationPresetProvider({
-    id: 'preset-interactions',
-    display: { name: 'Interactions' },
-    params: () => PresetParams,
-    async apply(ref, params, plugin) {
-        const structureCell = StateObjectRef.resolveAndCheck(plugin.state.data, ref);
-        const structure = structureCell?.obj?.data;
-        if (!structureCell || !structure) return {};
-
-        const components = {
-            ligand: await presetStaticComponent(plugin, structureCell, 'ligand'),
-            surroundings: await plugin.builders.structure.tryCreateComponentFromSelection(structureCell, ligandSurroundings, `surroundings`),
-            interactions: await presetStaticComponent(plugin, structureCell, 'ligand'),
-        };
-
-        const { update, builder, typeParams } = StructureRepresentationPresetProvider.reprBuilder(plugin, params);
-        const representations = {
-            ligand: builder.buildRepresentation(update, components.ligand, { type: 'ball-and-stick', typeParams: { ...typeParams, material: CustomMaterial, sizeFactor: 0.3 }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ligand' }),
-            ballAndStick: builder.buildRepresentation(update, components.surroundings, { type: 'ball-and-stick', typeParams: { ...typeParams, material: CustomMaterial, sizeFactor: 0.1, sizeAspectRatio: 1 }, color: 'element-symbol', colorParams: { carbonColor: { name: 'element-symbol', params: {} } } }, { tag: 'ball-and-stick' }),
-            interactions: builder.buildRepresentation(update, components.interactions, { type: InteractionsRepresentationProvider, typeParams: { ...typeParams, material: CustomMaterial, includeParent: true, parentDisplay: 'between' }, color: InteractionTypeColorThemeProvider }, { tag: 'interactions' }),
-            label: builder.buildRepresentation(update, components.surroundings, { type: 'label', typeParams: { ...typeParams, material: CustomMaterial, background: false, borderWidth: 0.1 }, color: 'uniform', colorParams: { value: Color(0x000000) } }, { tag: 'label' }),
-        };
-
-        await update.commit({ revertOnError: true });
-        await shinyStyle(plugin);
-        plugin.managers.interactivity.setProps({ granularity: 'element' });
-
-        return { components, representations };
-    }
-});
-
 export const ShowButtons = PluginConfig.item('showButtons', true);
 
 export class ViewportComponent extends PluginUIComponent {
-    // Handler for the input change event
-    handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        console.log('Input Value:', value);
-        // Add any additional logic you want to perform with the input value
-    };
 
-    // Handler for the input key press event
     handleInputKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === 'Enter') {
             const value = event.currentTarget.value;
             console.log('Enter pressed. Input Value:', value);
-            // Execute desired function with the input value
             this.processInput(value);
+            debugger;
         }
     };
 
-    // Custom function to process input
     processInput = (value: string) => {
-        // Define what you want to do with the input value
-        // For example, applying a custom representation based on user input
         console.log('Processing Input:', value);
-        // Example: Search for a specific residue or feature
-        // Implement your custom logic here
     };
 
     async _set(structures: readonly StructureRef[], preset: StructureRepresentationPresetProvider) {
@@ -255,10 +91,6 @@ export class ViewportComponent extends PluginUIComponent {
     };
 
     structurePreset = () => this.set(StructurePreset);
-    illustrativePreset = () => this.set(IllustrativePreset);
-    surfacePreset = () => this.set(SurfacePreset);
-    pocketPreset = () => this.set(PocketPreset);
-    interactionsPreset = () => this.set(InteractionsPreset);
 
     get showButtons() {
         return this.plugin.config.get(ShowButtons);
@@ -270,17 +102,15 @@ export class ViewportComponent extends PluginUIComponent {
         return <>
             <Viewport />
             {this.showButtons && <div className='msp-viewport-top-left-controls'>
-
-
                 <div
                     style={{
-                          position: 'fixed',
-                          bottom: 0,
-                          left: 0,
-                          width: '100%',
-                          padding: '8px',
-                          backgroundColor: '#fff',
-                          }}
+                        position: 'fixed',
+                        bottom: 0,
+                        left: 0,
+                        width: '100%',
+                        padding: '8px',
+                        backgroundColor: '#fff',
+                    }}
                 >
                     <input
                         type="text"
